905a0174c306d795b33649d67f2091a6fc3e12d0
alamin-shanto <mdalaminshanto1993@gmail.com>
2026-01-14 23:28:06 +0600

    Profile added

---
+++ src/data/profile.js
@@ -0,0 +1,15 @@
+export const profile = {
+  name: "Mohammad Al Amin",
+  designation: "Full Stack Developer (React, Node.js)",
+  photo:
+    "https://res.cloudinary.com/dn2wb32gr/image/upload/v1758128272/IMG_20250914_181249_sbmgtp.png",
+  resumeLink: "/Mohammad Resume.pdf",
+  email: "mash.dev02@gmail.com",
+  phone: "+8801986763961",
+  whatsapp: "+8801986763961",
+  social: {
+    github: "https://github.com/alamin-shanto",
+    linkedin: "https://www.linkedin.com/in/mash02/",
+    twitter: "https://x.com/mdalamin94402",
+  },
+};
